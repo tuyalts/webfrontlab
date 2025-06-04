37333698ff3a96e321bd6d9137cab5d23b283ea3
tuyalts <tuyana21lombo@gmail.com>
2025-06-04 17:40:12 +0700

    Завершение работы

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        count: '100 rub',
+        best: false,
+        availability: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        count: '150 rub',
+        best: false,
+        availability: false
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        count: '200 rub',
+        best: false,
+        availability: true
+    },
+    {
+        author: 'Франц Кафка',
+        name: 'Лабиринт',
+        publicationYear: 2023,
+        count: '299 rub',
+        best: true,
+        availability: true
+    },
+    {
+        author: 'Рейнор Винн',
+        name: 'Соленая тропа',
+        publicationYear: 2023,
+        count: '359 rub',
+        best: 'бестселлер: 1',
+        availability: true
+    },
+    {
+        author: 'Чехов',
+        name: 'Черный монах',
+        publicationYear: 2023,
+        count: '259 rub',
+        best: false,
+        availability: true
+    },
+    {
+        author: 'Рей Брэдбери',
+        name: 'Лекарство от меланхолии',
+        publicationYear: 2021,
+        count: '350 rub',
+        best: true,
+        availability: true
+    },
+    {
+        author: 'Харуки Мураками',
+        name: 'Хроники заводной птицы',
+        publicationYear: 2023,
+        count: '479 rub',
+        best: true,
+        availability: true
+    },
+    {
+        author: 'Ким Г. Д.',
+        name: 'Линейная алгебра и аналитическая геометрия',
+        publicationYear: 2008,
+        count: '200 rub',
+        best: false,
+        availability: false
+    },
+    {
+        author: 'Сергей Молчанов',
+        name: 'Бухучет за 14 дней',
+        publicationYear: 2023,
+        count: '400 rub',
+        best: false,
+        availability: true
+    }
+];
